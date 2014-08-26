40027d917cfb82216d86eaab6b814bc4fcf20959
raquelalegre <raquel.alegre@gmail.com>
2014-08-26 10:54:00 +0100

    if-then-else sentence added.

--- exercise1/1_create_compressed_file.sh
+++ exercise1/1_create_compressed_file.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
 # Print out a message in the shell
+echo "                                          "
 echo "Creating compressed file EO_images.zip ..."
+echo "                                          "
 
 # Create compressed file
-zip -v  EO_images.zip resources/*.jpg
+zip EO_images.zip resources/*.jpg 
 
-#Check if the operation went well and print an output message
+# Check if the operation went well and print an output message with some decorations so that it's easier to notice
+if [ $? -ne 0 ]; then
+	echo "                             "
+        echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        echo "!! Oops! Something went wrong :( !"
+        echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+else
+	echo "	                           "
+        echo "@@@@@@@@@@@"
+        echo "@ Done :) @"
+        echo "@@@@@@@@@@@"
+
+fi
+
+
 
